7471536a62d048887f321a1fa2cfe170bf0bde2e
Duong Pham <t0phtr00@students.oamk.fi>
2023-04-25 12:30:01 +0300

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -23,8 +23,8 @@ import socket from "./Socket"
 
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement)
 
-const URL = "http://localhost:8080/"
-
+const development_URL = "http://localhost:8080/"
+const production_URL = "https://air-quality.azurewebsites.net/"
 export const Dashboard = ({ historyData }) => {
   const navigate = useNavigate()
 
@@ -34,7 +34,7 @@ export const Dashboard = ({ historyData }) => {
   }
 
   const disconnect = () => {
-    fetch(`${URL}set-up/disconnection/`, {
+    fetch(`${production_URL}set-up/disconnection/`, {
       method: "POST",
     }).then((response) => {
       console.log("Disconnected")
@@ -57,12 +57,35 @@ export const Dashboard = ({ historyData }) => {
     sunrise: null,
     sunset: null,
   })
-
+  const [outdoorData, setOutdoorData] = useState({
+    aqi_outdoor: null,
+    app_temp: null,
+    temperature: null,
+    humidity: null,
+    wind_spd: null,
+    ob_time: null,
+    description: null,
+  })
   const [aqi, setAqi] = useState("")
   const [quality, setQuality] = useState(null)
   const [alerts, setAlerts] = useState([])
   const [currentTime, setCurrentTime] = useState(new Date())
   const [hiddenElement, setHiddenElement] = useState({})
+  const [time, setTime] = useState(new Date())
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+  const year = time.getFullYear()
+  const month = time.getMonth() + 1
+  const day = time.getDate()
+  const hours = time.getHours()
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+
   //const [index,setIndex]=useState["#FAD200","#FA1D00","#FA1D00"]
   function handleDismiss() {
     //setAlerts([])
@@ -75,6 +98,15 @@ export const Dashboard = ({ historyData }) => {
       const temporary = JSON.parse(data)
       const { light, co2, tvoc, humd, airp, temp, sunrise, sunset } =
         temporary.elements
+      const {
+        aqi_outdoor,
+        app_temp,
+        temperature,
+        humidity,
+        wind_spd,
+        ob_time,
+        description,
+      } = temporary.outdoor
       const aqi = temporary.AQI
       const quality = temporary.Quality
       const alerts = temporary.alerts
@@ -89,14 +121,47 @@ export const Dashboard = ({ historyData }) => {
         sunrise,
         sunset,
       })
+      setOutdoorData({
+        aqi_outdoor,
+        app_temp,
+        temperature,
+        humidity,
+        wind_spd,
+        ob_time,
+        description,
+      })
       setHiddenElement({})
       setAqi(aqi)
       setQuality(quality)
       setAlerts(alerts)
 
-      // localStorage.setItem("realtimeData", JSON.stringify(realtimeData))
-      // localStorage.setItem("aqi", aqi)
-      // localStorage.setItem("quality", quality)
+      localStorage.setItem(
+        "indoorData",
+        JSON.stringify({
+          light,
+          co2,
+          tvoc,
+          humd,
+          airp: Math.round(airp),
+          temp,
+          sunrise,
+          sunset,
+        })
+      )
+      localStorage.setItem("aqi", aqi)
+      localStorage.setItem("quality", quality)
+      localStorage.setItem(
+        "outdoorData",
+        JSON.stringify({
+          aqi_outdoor,
+          app_temp,
+          temperature,
+          humidity,
+          wind_spd,
+          ob_time,
+          description,
+        })
+      )
     })
 
     // return () => {}
@@ -109,11 +174,14 @@ export const Dashboard = ({ historyData }) => {
   }
 
   const getDataFromStorage = () => {
-    const storedData = localStorage.getItem("realtimeData")
+    let storedData = localStorage.getItem("indoorData")
     if (storedData) {
       setRealtimeData(JSON.parse(storedData))
     }
-
+    storedData = localStorage.getItem("outdoorData")
+    if (storedData) {
+      setOutdoorData(JSON.parse(storedData))
+    }
     const storedAqi = localStorage.getItem("aqi")
     const storedQuality = localStorage.getItem("quality")
     if (storedAqi && storedQuality) {
@@ -129,7 +197,7 @@ export const Dashboard = ({ historyData }) => {
   const [tempData, setTempData] = useState([])
 
   const fetchTempData = () => {
-    fetch(`${URL}history`)
+    fetch(`${production_URL}history`)
       .then((response) => response.json())
       .then((data) => {
         setTempData(data.list)
@@ -165,7 +233,15 @@ export const Dashboard = ({ historyData }) => {
     : null
   const humdAlert = alerts.find((alert) => alert.element.includes("humidity"))
   const humdStyle = humdAlert ? { background: humdAlert.index } : null
-  console.log(co2Style)
+
+  const qualityStyle =
+    quality == "Excellent"
+      ? null
+      : quality == "Good"
+      ? { color: "green" }
+      : quality == "Moderate"
+      ? { color: "orange" }
+      : null
 
   return (
     <div className="bg-[#F8F8FF] h-screen">
@@ -193,25 +269,30 @@ export const Dashboard = ({ historyData }) => {
         </div>
       )}
 
-      <div className="py-12 px-32">
-        <div className="flex justify-between">
-          <div className="text-light-purple font-bold text-5xl font-sans mb-12">
-            HALTIAN DEMO
-          </div>
-          <div className="w-[10%]">
+      <div className="pt-4 px-32 h-screen">
+        <div className="text-light-purple font-bold text-5xl font-sans mb-12 text-center">
+          INDOOR AIR QUALITY MORNITORING
+          {/* <div className="w-[20%] flex justify-between">
             <button
-              className="rounded-xl bg-light-purple text-white h-[50%] w-full"
+              className="rounded-xl bg-light-purple text-white h-[50%] w-[40%]"
+              onClick={navigateHistory}
+            >
+              History
+            </button>
+            <button
+              className="rounded-xl bg-light-purple text-white h-[50%] w-[40%]"
               onClick={navigateConnection}
             >
               Disconnect
             </button>
-          </div>
+
+          </div> */}
         </div>
 
-        <div className="flex justify-between mb-16">
-          <div className="bg-white rounded-xl flex flex-col w-[25%] justify-around">
+        <div className="flex justify-between mb-16 h-[50%]">
+          <div className="bg-white rounded-xl flex flex-col w-[30%] justify-around">
             <div className="text-center text-light-purple text-3xl">
-              AIR QUALITY
+              AIR QUALITY INDEX
             </div>
 
             <CircularProgressbar
@@ -225,12 +306,27 @@ export const Dashboard = ({ historyData }) => {
               })}
             />
 
-            <div className="text-center text-light-purple text-3xl font-semibold">
+            <div
+              className="text-center text-light-purple text-3xl font-semibold"
+              style={qualityStyle}
+            >
               {quality || "Analyzing"}
+            </div>
+            <div className="text-blue-600/50 text-1xl font-semibold">
+              Last updated:{" "}
+              {currentTime.toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+                hour12: false,
+                // weekday: 'short',
+                // month: 'short',
+                // day: 'numeric',
+              })}
             </div>
           </div>
 
-          <div className="w-[28%]">
+          <div className="w-[30%]">
             <div
               className="bg-white rounded-xl flex py-4 mb-8"
               style={co2Style}
@@ -280,7 +376,7 @@ export const Dashboard = ({ historyData }) => {
             </div>
           </div>
 
-          <div className="w-[28%]">
+          <div className="w-[30%]">
             <div className="bg-white rounded-xl flex py-4 mb-8">
               <div className="flex align-middle justify-center w-[50%]">
                 <img
@@ -324,10 +420,10 @@ export const Dashboard = ({ historyData }) => {
           </div>
         </div>
 
-        <div className="flex justify-between">
-          <div className="w-[35%] rounded-xl">
+        <div className="flex justify-between h-[40%]">
+          <div className="w-[35%] rounded-xl flex flex-col justify-between ">
             <div className="flex bg-white rounded-xl justify-around mb-8">
-              <div className="flex w-[40%] py-8">
+              <div className="flex p-8">
                 <div className="border-r-2 border-r-black">
                   <div className="flex pb-4" style={humdStyle}>
                     <img className="w-[30%] " src="humidity.png" alt="img" />
@@ -346,7 +442,7 @@ export const Dashboard = ({ historyData }) => {
                 </div>
               </div>
 
-              <div className="flex w-[40%] py-8">
+              <div className="flex  p-8">
                 <div>
                   <div className="flex pb-4">
                     <img className="w-[30%] " src="sunrise.png" alt="img" />
@@ -369,32 +465,164 @@ export const Dashboard = ({ historyData }) => {
                 </div>
               </div>
             </div>
-            <div className="bg-[#C3CAFF] rounded-xl p-8 text-3xl text-white font-semibold">
-              {/* Suggestion */}
-              Last updated:{" "}
-              {currentTime.toLocaleString("en-US", {
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric",
+            {/* <div className="bg-[#C3CAFF] rounded-xl p-8 text-3xl text-black font-semibold "> */}
+            {/* Suggestion */}
+            {/* Last updated: {currentTime.toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
                 hour12: false,
                 // weekday: 'short',
                 // month: 'short',
                 // day: 'numeric',
-              })}
-            </div>
-          </div>
-          <div className="bg-white w-[60%] rounded-xl p-8 h-[40%]">
-            <div className="flex justify-between">
-              <div className="font-semibold">Previous Temperature</div>
-              <button
-                className="w-[15%] p-2 bg-light-purple text-white rounded-full"
-                onClick={navigateHistory}
+              })} */}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                border: "1px solid black",
+                borderRadius: "5px",
+              }}
+              className="p-8 text-3xl text-black font-semibold bg-[#C3CAFF] rounded-xl"
+            >
+              <div
+                style={{
+                  height: "auto",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: "2.5rem",
+                }}
+                className="bg-[#C3CAFF] rounded-xl "
               >
-                History
-              </button>
+                {`${hours < 10 ? "0" : ""}${hours}:${
+                  minutes < 10 ? "0" : ""
+                }${minutes}:${seconds < 10 ? "0" : ""}${seconds} ${
+                  day < 10 ? "0" : ""
+                }${day}/${month < 10 ? "0" : ""}${month}/${year} `}
+              </div>
             </div>
+            {/* </div> */}
+          </div>
+          <div className="bg-white w-[60%] rounded-xl p-8 ">
+            <div className="text-center text-5xl	">
+              <div className="font-semibold">OUTDOOR WEATHER</div>
+            </div>
+            <div className="flex bg-white rounded-xl justify-around h-[80%] items-center">
+              <div className="flex w-[40%] p-8 m-auto">
+                <div className="border-r-2 border-r-black">
+                  <div className="flex pb-4">
+                    <svg
+                      className="h-16 w-16 text-indigo-500"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      {" "}
+                      <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                      <polyline points="4 19 8 13 12 15 16 10 20 14 20 19 4 19" />{" "}
+                      <polyline points="4 12 7 8 11 10 16 4 20 8" />
+                    </svg>
+                    <div className="align-center  ml-7 my-auto">
+                      <div>Air Quality Index</div>
+                      <div className="font-bold">
+                        {outdoorData.aqi_outdoor || "Updating"}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    {/* <img className="w-[30%] " src="lamp.png" alt="img" />
+                    <div className="align-center m-auto">
+                      <div>Light Level</div>
+                      <div className="font-bold">{realtimeData.light || 0}</div>
+                    </div> */}
+                    <div className="align-center m-auto ">
+                      <div className="font-bold">
+                        {outdoorData.description || ""}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-[40%] p-8">
+                <div className="border-r-2 border-r-black">
+                  <div className="flex pb-4">
+                    <img className="w-[30%] " src="thermometer.png" alt="img" />
+                    <div className="align-center  ml-7 my-auto">
+                      <div>Temperature</div>
+                      <div className="font-bold">
+                        {outdoorData.temperature} Cel
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div>
+                      <svg
+                        className="h-16 w-16 text-indigo-500"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        stroke-width="2"
+                        stroke="currentColor"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        {" "}
+                        <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                        <circle cx="12" cy="7" r="4" />{" "}
+                        <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                      </svg>
+                    </div>
 
-            <ResponsiveContainer width="100%" height={350}>
+                    <div className="align-center ml-7 my-auto">
+                      <div>Feels like</div>
+                      <div className="font-bold">
+                        {outdoorData.app_temp || "Updating"} Cel
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-[40%] p-8">
+                <div>
+                  <div className="flex pb-4">
+                    <img className="w-[30%] " src="humidity.png" alt="img" />
+                    <div className="align-center  ml-7 my-auto">
+                      <div>Humidity</div>
+                      <div className="font-bold">
+                        {outdoorData.humidity || "Updating"}%
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <svg
+                      className="h-16 w-16 text-indigo-500"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      strokeLinecap="round"
+                      stroke-linejoin="round"
+                    >
+                      {" "}
+                      <path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2" />
+                    </svg>
+                    <div className="align-center ml-7 my-auto">
+                      <div>Wind speed</div>
+                      <div className="font-bold">
+                        {outdoorData.wind_spd || "Updating"} m/s
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* <ResponsiveContainer width="100%" height={350}>
               <LineChart data={temperatureData}>
                 <XAxis dataKey="Time" />
                 <YAxis />
@@ -403,7 +631,11 @@ export const Dashboard = ({ historyData }) => {
                 <Legend />
                 <Line type="monotone" dataKey="Temperature" stroke="#8884d8" />
               </LineChart>
-            </ResponsiveContainer>
+            </ResponsiveContainer> */}
+
+            <div className="text-center">
+              Last observation time: {outdoorData.ob_time || "Updating"}
+            </div>
           </div>
         </div>
       </div>
