ffe659a4a4b83a12b7e3f6a07e30013f14259232
Duong Pham <t0phtr00@students.oamk.fi>
2023-04-25 17:02:21 +0300

    Update History.js

--- src/pages/History.js
+++ src/pages/History.js
@@ -52,55 +52,61 @@ export const History = () => {
 
   return (
     <div className="bg-[#F8F8FF] h-screen flex items-center">
-      <div className="flex flex-col justify-between mx-auto h-[90%] w-[90%]">
+      <div className="flex flex-col justify-between mx-auto h-[85%] w-[90%]">
         <div className="text-3vw text-light-purple font-bold mx-auto">
           HALTIAN DEMO
         </div>
-        <div className="bg-white rounded-xl w-[80%] h-[80%] mx-auto p-8">
-          <div className="flex py-8 justify-between">
-            <div className="text-2xl font-semibold">History Data</div>
-            <button
-              className="w-[10%] bg-light-purple text-white rounded-full"
-              onClick={navigateDashboard}
-            >
-              Back
-            </button>
-          </div>
-          <div className="grid grid-cols-2 grid-rows-2">
-            <ResponsiveContainer width="200%" height={300}>
-              <LineChart data={temperatureData}>
-                <XAxis dataKey="Time" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="Temperature" stroke="#8884d8" />
-              </LineChart>
-            </ResponsiveContainer>
+        <div className="bg-white justify-around rounded-xl w-[80%] h-[85%] flex-col flex mx-auto items-center">
+          <div className="mx-auto h-[90%] w-[95%] justify-between flex flex-col">
+            <div className="flex justify-between w-full">
+              <div className="text-1.5vw font-semibold">History Data</div>
+              <button
+                className="w-[10%] bg-light-purple text-white rounded-full text-1vw px-[2%] py-[%]"
+                onClick={navigateDashboard}
+              >
+                Back
+              </button>
+            </div>
+            <div className="grid grid-cols-2 grid-rows-2 h-[90%]">
+              <ResponsiveContainer width="200%">
+                <LineChart data={temperatureData}>
+                  <XAxis dataKey="Time" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend />
+                  <Line
+                    type="monotone"
+                    dataKey="Temperature"
+                    stroke="#8884d8"
+                  />
+                </LineChart>
+              </ResponsiveContainer>
 
-            <div></div>
+              <div></div>
 
-            <ResponsiveContainer>
-              <LineChart data={humidityData}>
-                <XAxis dataKey="Time" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="Humidity" stroke="#82ca9d" />
-              </LineChart>
-            </ResponsiveContainer>
+              <ResponsiveContainer>
+                <LineChart data={humidityData}>
+                  <XAxis dataKey="Time" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="Humidity" stroke="#82ca9d" />
+                </LineChart>
+              </ResponsiveContainer>
 
-            <ResponsiveContainer>
-              <LineChart data={co2Data}>
-                <XAxis dataKey="Time" />
-                <YAxis />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="CO2" stroke="#ffc658" />
-              </LineChart>
-            </ResponsiveContainer>
+              <ResponsiveContainer>
+                <LineChart data={co2Data}>
+                  <XAxis dataKey="Time" />
+                  <YAxis />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend />
+                  <Line type="monotone" dataKey="CO2" stroke="#ffc658" />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
           </div>
         </div>
       </div>
